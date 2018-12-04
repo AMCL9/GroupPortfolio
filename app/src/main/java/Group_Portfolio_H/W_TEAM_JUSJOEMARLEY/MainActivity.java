6e60fb2aefe9868de4ad270247da46f127ccc8da
Ohms TheResistance <ohmstheresistance@Ohmss-MacBook-Pro.local>
2018-12-03 21:16:35 -0500

    First Commit

---
+++ app/src/main/java/Group_Portfolio_H/W_TEAM_JUSJOEMARLEY/MainActivity.java
@@ -0,0 +1,13 @@
+package Group_Portfolio_H.W_TEAM_JUSJOEMARLEY;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
